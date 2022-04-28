c463bf21814750c440a7220bc0094dcd46703394
Youggls <462046122@qq.com>
2022-04-28 21:57:45 +0800

    Update

--- task1/run.sh
+++ task1/run.sh
@@ -1,18 +1,32 @@
 ./main-huawei -s false -d 512 -h 2048 -f 256 -o 32 -t 0
 ./main-huawei -s false -d 512 -h 2048 -f 256 -o 32 -t 0
+
 ./main-huawei -s false -d 512 -h 2048 -f 512 -o 32 -t 0
 ./main-huawei -s true -d 512 -h 2048 -f 512 -o 32 -t 0
-./main-huawei -s false -d 512 -h 2048 -f 256 -o 32 -t 0
-./main-huawei -s true -d 512 -h 2048 -f 256 -o 32 -t 0
-./main-huawei -s false -d 512 -h 4096 -f 512 -o 32 -t 0
-./main-huawei -s true -d 512 -h 4096 -f 512 -o 32 -t 0
+
 ./main-huawei -s false -d 512 -h 4096 -f 256 -o 32 -t 0
 ./main-huawei -s true -d 512 -h 4096 -f 256 -o 32 -t 0
-./main-huawei -s false -d 1024 -h 4096 -f 512 -o 32 -t 0
-./main-huawei -s true -d 1024 -h 4096 -f 512 -o 32 -t 0
+
+./main-huawei -s false -d 512 -h 4096 -f 512 -o 32 -t 0
+./main-huawei -s true -d 512 -h 4096 -f 512 -o 32 -t 0
+
 ./main-huawei -s false -d 1024 -h 4096 -f 256 -o 32 -t 0
 ./main-huawei -s true -d 1024 -h 4096 -f 256 -o 32 -t 0
-./main-huawei -s false -d 2048 -h 4096 -f 512 -o 32 -t 0
-./main-huawei -s true -d 2048 -h 4096 -f 512 -o 32 -t 0
+
+./main-huawei -s false -d 1024 -h 4096 -f 512 -o 32 -t 0
+./main-huawei -s true -d 1024 -h 4096 -f 512 -o 32 -t 0
+
 ./main-huawei -s false -d 2048 -h 4096 -f 256 -o 32 -t 0
 ./main-huawei -s true -d 2048 -h 4096 -f 256 -o 32 -t 0
+
+./main-huawei -s false -d 2048 -h 4096 -f 512 -o 32 -t 0
+./main-huawei -s true -d 2048 -h 4096 -f 512 -o 32 -t 0
+
+./main-huawei -s false -d 2048 -h 8196 -f 256 -o 32 -t 0
+./main-huawei -s true -d 2048 -h 8196 -f 256 -o 32 -t 0
+
+./main-huawei -s false -d 2048 -h 8196 -f 512 -o 32 -t 0
+./main-huawei -s true -d 2048 -h 8196 -f 512 -o 32 -t 0
+
+./main-huawei -s false -d 2048 -h 8196 -f 1024 -o 32 -t 0
+./main-huawei -s true -d 2048 -h 8196 -f 1024 -o 32 -t 0
