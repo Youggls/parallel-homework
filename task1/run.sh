56e6e276379c1fcf01205df7fcf8ccd46ef571d1
Youggls <462046122@qq.com>
2022-04-29 17:29:43 +0800

    Update: update run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,34 +1,45 @@
 exec_name="main-huawei"
 
-./${exec_name} -s false -d 512 -h 2048 -f 256 -o 32 -t 0
-./${exec_name} -s true -d 512 -h 2048 -f 256 -o 32 -t 0
+./${exec_name} -s false -c false -d 512 -h 2048 -f 256 -o 32 -t 0
+./${exec_name} -s false -c true -d 512 -h 2048 -f 256 -o 32 -t 0
+./${exec_name} -s true -c false -d 512 -h 2048 -f 256 -o 32 -t 0
 
-./${exec_name} -s false -d 512 -h 2048 -f 512 -o 32 -t 0
-./${exec_name} -s true -d 512 -h 2048 -f 512 -o 32 -t 0
+./${exec_name} -s false -c false -d 512 -h 2048 -f 512 -o 32 -t 0
+./${exec_name} -s false -c true -d 512 -h 2048 -f 512 -o 32 -t 0
+./${exec_name} -s true -c false -d 512 -h 2048 -f 512 -o 32 -t 0
 
-./${exec_name} -s false -d 512 -h 4096 -f 256 -o 32 -t 0
-./${exec_name} -s true -d 512 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s false -c false -d 512 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s false -c true -d 512 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s true -c false -d 512 -h 4096 -f 256 -o 32 -t 0
 
-./${exec_name} -s false -d 512 -h 4096 -f 512 -o 32 -t 0
-./${exec_name} -s true -d 512 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s false -c false -d 512 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s false -c true -d 512 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s true -c false -d 512 -h 4096 -f 512 -o 32 -t 0
 
-./${exec_name} -s false -d 1024 -h 4096 -f 256 -o 32 -t 0
-./${exec_name} -s true -d 1024 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s false -c false -d 1024 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s false -c true -d 1024 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s true -c false -d 1024 -h 4096 -f 256 -o 32 -t 0
 
-./${exec_name} -s false -d 1024 -h 4096 -f 512 -o 32 -t 0
-./${exec_name} -s true -d 1024 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s false -c false -d 1024 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s false -c true -d 1024 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s true -c false -d 1024 -h 4096 -f 512 -o 32 -t 0
 
-./${exec_name} -s false -d 2048 -h 4096 -f 256 -o 32 -t 0
-./${exec_name} -s true -d 2048 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s false -c false -d 2048 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s false -c true -d 2048 -h 4096 -f 256 -o 32 -t 0
+./${exec_name} -s true -c false -d 2048 -h 4096 -f 256 -o 32 -t 0
 
-./${exec_name} -s false -d 2048 -h 4096 -f 512 -o 32 -t 0
-./${exec_name} -s true -d 2048 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s false -c false -d 2048 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s false -c true -d 2048 -h 4096 -f 512 -o 32 -t 0
+./${exec_name} -s true -c false -d 2048 -h 4096 -f 512 -o 32 -t 0
 
-./${exec_name} -s false -d 2048 -h 8196 -f 256 -o 32 -t 0
-./${exec_name} -s true -d 2048 -h 8196 -f 256 -o 32 -t 0
+./${exec_name} -s false -c false -d 2048 -h 8196 -f 256 -o 32 -t 0
+./${exec_name} -s false -c true -d 2048 -h 8196 -f 256 -o 32 -t 0
+./${exec_name} -s true -c false -d 2048 -h 8196 -f 256 -o 32 -t 0
 
-./${exec_name} -s false -d 2048 -h 8196 -f 512 -o 32 -t 0
-./${exec_name} -s true -d 2048 -h 8196 -f 512 -o 32 -t 0
+./${exec_name} -s false -c false -d 2048 -h 8196 -f 512 -o 32 -t 0
+./${exec_name} -s false -c true -d 2048 -h 8196 -f 512 -o 32 -t 0
+./${exec_name} -s true -c false -d 2048 -h 8196 -f 512 -o 32 -t 0
 
-./${exec_name} -s false -d 2048 -h 8196 -f 1024 -o 32 -t 0
-./${exec_name} -s true -d 2048 -h 8196 -f 1024 -o 32 -t 0
+./${exec_name} -s false -c false -d 2048 -h 8196 -f 1024 -o 32 -t 0
+./${exec_name} -s false -c true -d 2048 -h 8196 -f 1024 -o 32 -t 0
+./${exec_name} -s true -c false -d 2048 -h 8196 -f 1024 -o 32 -t 0
